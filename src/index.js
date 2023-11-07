2c43ab6bb6b37dae69051436698aff0dc5600377
Silvia <silviatc1993@gmail.com>
2023-11-07 19:03:12 +0100

    working on URL from our server

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ server.use(cors());
 server.use(express.json({ limit: '25mb' }));
 server.set('view engine', 'ejs');
 
-const pool = mysql.createPool({
-  host: 'sql.freedb.tech',
-  database: 'freedb_proyectos_molones',
-  user: 'freedb_hierbas',
-  password: 'eD8qhz*taP#Ja%w',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'sql.freedb.tech',
+    database: 'freedb_proyectos_molones',
+    user: 'freedb_hierbas',
+    password: 'eD8qhz*taP#Ja%w',
+  });
+  connection.connect();
+  return connection;
+}
 
 const port = 2002;
 server.listen(port, () => {
@@ -23,35 +24,25 @@ server.listen(port, () => {
 });
 
 server.get('/project', async (req, res) => {
-  try {
-    const sql =
-      'SELECT user.name AS autor, user.image_user AS image , project.name_project AS name, project.slogan, project.repo, project.demo, project.tech, project.desc FROM user INNER JOIN project ON user.iduser = project.fk_user';
-
-    const [results] = await pool.query(sql);
-    res.json({
-      success: true,
-      projects: results,
-    });
-  } catch (error) {
-    console.error('Error al obtener proyectos:', error);
-    res
-      .status(500)
-      .json({ success: false, error: 'Error al obtener proyectos.' });
-  }
+  const sql =
+    'SELECT user.name AS autor, user.image_user AS image , project.name_project AS name, project.slogan, project.repo, project.demo, project.tech, project.desc FROM user INNER JOIN project ON user.iduser = project.fk_user';
+  const connection = await getConnection();
+  const [results] = await connection.query(sql);
+  connection.end();
+  res.json({
+    success: true,
+    projects: results,
+  });
 });
 
 server.post('/createproject', async (req, res) => {
   const body = req.body;
 
   try {
-    const connection = await pool.getConnection();
+    const connection = await getConnection();
 
     const insertUser =
       'INSERT INTO user (name, job, image_user) VALUES (?, ?, ?);';
-
-    console.log('body.name > ', body.name);
-    console.log('body.job > ', body.job);
-    console.log('body.image_user > ', body.image_user);
 
     const [resultUser] = await connection.query(insertUser, [
       body.name,
